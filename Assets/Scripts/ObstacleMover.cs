268c8756f783240f9639497b49c86a91f842f0e0
max-acebal <60718877+max-acebal@users.noreply.github.com>
2020-03-16 15:20:04 -0400

    Add files via upload

---
+++ Assets/Scripts/ObstacleMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMover : MonoBehaviour {
+
+    public float speedPerSecond = 10f;
+    public GameObject endpoint;
+
+	// Use this for initialization
+	void Start ()
+    {
+        endpoint = GameObject.FindGameObjectWithTag("Endpoint");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        float speed = speedPerSecond * Time.deltaTime;
+        Vector3 path = Vector3.MoveTowards(transform.position, endpoint.transform.position, speed);
+        path.x = transform.position.x;
+        path.y = transform.position.y;
+        transform.position = path;
+	}
+}
